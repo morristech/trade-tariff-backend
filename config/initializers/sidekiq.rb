acc5e8806ee911ca554cf3ffe364df58ef1daea5
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-02-21 17:33:49 +0300

    [SIDEKIQ] corrected setup file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,13 @@
 require "sidekiq"
 
 # PaaS Redis is not ready for prod
-if TradeTariffBackend.production?
-  redis_url = ENV["REDIS_URL"]
-  # db > 1 does not work for old redis service, but when we will switch to PaaS redis it should work
-  redis_db = 0
+if ENV['VCAP_SERVICES'].present?
+  redis_url = JSON.parse(ENV["VCAP_SERVICES"])["redis"].select{ |s| s["name"] == ENV["REDIS_INSTANCE_NAME"] }[0]["credentials"]["uri"]
 else
-  redis_url = begin
-    JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
-  rescue
-    ENV["REDIS_URL"]
-  end
-  redis_db = ENV["REDIS_DB"] || 0
+  redis_url = ENV["REDIS_URL"]
 end
+
+redis_db = ENV["REDIS_DB"] || 0
 
 redis_config = { url: redis_url, db: redis_db }
 
